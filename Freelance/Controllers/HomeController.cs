3d528529b71960f8c20d382e7cadfc5496086b95
bplokarz226060 <plokarzbartlomiej@gmail.com>
2018-07-11 19:52:03 +0200

    Set IoC container.

--- Freelance/Controllers/HomeController.cs
+++ Freelance/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Freelance.Core.Repositories;
 
 namespace Freelance.Controllers
 {
     public class HomeController : Controller
     {
+        private IServiceTypeRepository _serviceTypeRepository;
+
+        public HomeController(IServiceTypeRepository serviceTypeRepository)
+        {
+            _serviceTypeRepository = serviceTypeRepository;
+        }
+
         public ActionResult Index()
         {
+            _serviceTypeRepository.GetAllAsync();
             return View();
         }
 
---
+++ Freelance/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Freelance.Core.Models;
+using Freelance.Core.Repositories;
+using Freelance.Infrastructure.Repositories;
+using Ninject;
+using Ninject.Web.Common;
+
+namespace Freelance.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IAnnouncementRepository>().To<AnnouncementRepository>().InRequestScope();
+            kernel.Bind<IServiceTypeRepository>().To<ServiceTypeRepository>().InRequestScope();
+
+            kernel.Bind<ApplicationDbContext>().ToSelf().InRequestScope();
+        }
+    }
+}
