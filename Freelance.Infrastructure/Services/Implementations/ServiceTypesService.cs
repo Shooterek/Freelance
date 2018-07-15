d1e790e49148d55bb423655f034f5cb2f33d7087
bplokarz226060 <plokarzbartlomiej@gmail.com>
2018-07-15 14:48:49 +0200

    Added ServiceTypesService

---
+++ Freelance.Infrastructure/Services/Implementations/ServiceTypesService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Freelance.Core.Models;
+using Freelance.Core.Repositories;
+using Freelance.Infrastructure.Services.Interfaces;
+
+namespace Freelance.Infrastructure.Services.Implementations
+{
+    public class ServiceTypesService : IServiceTypesService
+    {
+        private IServiceTypesRepository _serviceTypesRepository;
+
+        public ServiceTypesService(IServiceTypesRepository serviceTypesRepository)
+        {
+            _serviceTypesRepository = serviceTypesRepository;
+        }
+
+        public async Task<ICollection<ServiceType>> GetServiceTypesAsync()
+        {
+            var result = await _serviceTypesRepository.GetAllAsync();
+
+            return result.Entity;
+        }
+
+        public async Task<ServiceType> GetServiceTypeByIdAsync(int serviceTypeId)
+        {
+            var result = await _serviceTypesRepository.GetByIdAsync(serviceTypeId);
+
+            return result.Status == RepositoryStatus.Ok ? result.Entity : null;
+        }
+
+        public async Task AddServiceTypeAsync(ServiceType serviceType)
+        {
+            await _serviceTypesRepository.AddAsync(serviceType);
+        }
+
+        public async Task RemoveServiceTypeAsync(int serviceTypeId)
+        {
+            await _serviceTypesRepository.RemoveAsync(serviceTypeId);
+        }
+    }
+}
---
+++ Freelance.Infrastructure/Services/Interfaces/IServiceTypesService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Freelance.Core.Models;
+
+namespace Freelance.Infrastructure.Services.Interfaces
+{
+    public interface IServiceTypesService
+    {
+        Task<ICollection<ServiceType>> GetServiceTypesAsync();
+        Task<ServiceType> GetServiceTypeByIdAsync(int serviceTypeId);
+        Task AddServiceTypeAsync(ServiceType serviceType);
+        Task RemoveServiceTypeAsync(int serviceTypeId);
+    }
+}
