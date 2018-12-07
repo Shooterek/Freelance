8b9d4da3c6caa34e8f00a4e18be5076573337821
Bartek <Bartek@DESKTOP-BGP5G1J>
2018-12-08 00:22:20 +0100

    Added errors displaying in login view

--- Freelance/Controllers/AccountController.cs
+++ Freelance/Controllers/AccountController.cs
@@ -96,7 +96,7 @@ namespace Freelance.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Błędny login lub hasło");
                     return View(model);
             }
         }
