e6ee230a02228ead16d005e95f68b37fb99d13d6
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2020-09-27 23:58:28 +0200

    Upade the Seed method.

--- Freelance.Core/Migrations/Configuration.cs
+++ Freelance.Core/Migrations/Configuration.cs
@@ -18,32 +18,11 @@ namespace Freelance.Core.Migrations
 
         protected override void Seed(Freelance.Core.Models.ApplicationDbContext context)
         {
-            if (context.Users.Any(u => u.UserName == "plokarzbartlomiej@gmail.com"))
+            if (!context.Roles.Any())
             {
-//                //Step 1 Create the user.
-//                var passwordHasher = new PasswordHasher();
-//                var user = new ApplicationUser
-//                {
-//                    UserName = "plokarzbartlomiej@gmail.com",
-//                    Email = "plokarzbartlomiej@gmail.com",
-//                    PasswordHash = passwordHasher.HashPassword("!23Qwe"),
-//                    SecurityStamp = Guid.NewGuid().ToString()
-//                };
-//            
-//                //Step 2 Create and add the new Role.
-//                var roleToChoose = new IdentityRole("Admin");
-//                context.Roles.Add(roleToChoose);
-//            
-//                //Step 3 Create a role for a user
-//                var role = new IdentityUserRole
-//                {
-//                    RoleId = roleToChoose.Id,
-//                    UserId = user.Id
-//                };
-//            
-//                //Step 4 Add the role row and add the user to DB)
-//                user.Roles.Add(role);
-//                context.Users.Add(user);
+                //Step 2 Create and add the new Role.
+                var roleToChoose = new IdentityRole("Admin");
+                context.Roles.Add(roleToChoose);
             }
         }
     }
--- Freelance/Controllers/AccountController.cs
+++ Freelance/Controllers/AccountController.cs
@@ -178,6 +178,10 @@ namespace Freelance.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    if (user.UserName.Equals("admin@admin.com"))
+                    {
+                        await UserManager.AddToRoleAsync(user.Id, "Admin");
+                    }
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
